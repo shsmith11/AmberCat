093e4fe825a67e31d86ea23d50642ae4bab06e11
Sergey Kuznetsov <SKuznetsov@Attentigroup.com>
2021-10-12 18:54:02 -0400

    report added

--- src/test/java/cucumberOptions/TestNGRunner.java
+++ src/test/java/cucumberOptions/TestNGRunner.java
@@ -6,8 +6,12 @@ import io.cucumber.testng.CucumberOptions;
 
 
 
-@CucumberOptions(features = "src/test/java/features",
-glue = "stepDefinitions", tags = "@MobileTest or @RegTest")
+@CucumberOptions(
+        features = "src/test/java/features",
+        glue = "stepDefinitions",
+        tags = "@MobileTest or @RegTest",
+        monochrome = true, // detailed report in output
+        plugin = {"pretty", "html:target/cucumber.html"})  //pretty activates html report
 public class TestNGRunner extends AbstractTestNGCucumberTests {
 
 
--- src/test/java/cucumberOptions/jUnitTestRunner.java
+++ src/test/java/cucumberOptions/jUnitTestRunner.java
@@ -11,7 +11,9 @@ import org.junit.runner.RunWith;
             features = "src/test/java/features",
             glue = "stepDefinitions",
             stepNotifications = true,
-            tags = "@RegTest and @SmokeTest")
+            tags = "@RegTest and @SmokeTest",
+            plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json", "junit:target/cukes.xml"})  //pretty activates html report
+
     public class jUnitTestRunner {
 
 }
